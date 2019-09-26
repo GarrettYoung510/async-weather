3631b58eeadf65ab459e90bacd09d5c2887e4212
Garrett Young <garrettyoung510@gmail.com>
2019-09-25 23:44:45 -0700

    added https

--- js/forecast.js
+++ js/forecast.js
@@ -5,7 +5,7 @@ const key = 'FhN4dTJ0i0jYVqMgrIT6ApoAxmY4kStX';
 
 // get weather info
 const getWeather = async (id) => {
-    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const base = 'https://dataservice.accuweather.com/currentconditions/v1/';
     const query = `${id}?apikey=${key}`;
 
     const response = await fetch(base + query);
@@ -17,7 +17,7 @@ const getWeather = async (id) => {
 
 // get city info
 const getCity = async (city) => {
-    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const base = 'https://dataservice.accuweather.com/locations/v1/cities/search';
     const query = `?apikey=${key}&q=${city}`;
 
     const response = await fetch(base + query);
